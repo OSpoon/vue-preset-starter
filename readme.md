92eb727e8286fd72e5334d9b78273e1358590420
张鑫 <zxin088@gmail.com>
2020-02-26 10:44:48 +0800

    完成第一版模板项目

---
+++ readme.md
@@ -0,0 +1,11 @@
+## vue-preset-starter
+
+### Usage
+
+```sh
+vue create --preset ospoon/vue-preset-starter <project-name>
+```
+注意修改上面的 project-name 为要创建的文件夹名称
+
+### Preset Config
+[preset.json](./preset.json)
---
+++ template/src/views/home/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    Home
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {},
+  data() {
+    return {}
+  },
+  created() {},
+  mounted() {},
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
